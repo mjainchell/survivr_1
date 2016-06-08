ebaf008c8122020bab1d905b2848e5a1dba273fb
Mark Jainchell <mjainchell@yahoo.com>
2016-06-07 23:31:40 -0400

    GEM built and is working as expected

--- app.rb
+++ app.rb
@@ -1,1 +1,8 @@
-require 'colorizr'
+require 'colorizr.rb'
+
+p String.colors
+
+puts String.sample_colors
+
+puts 'Hello World!'.red
+
---
+++ lib/colorizr.rb
@@ -0,0 +1,37 @@
+class String
+
+  @@the_colors = {:red => 31, :green => 32 , :yellow => 33, :blue => 34, :pink => 35, :light_blue => 94, :white => 97, :light_grey => 37, :black => 30}
+
+  def self.create_colors
+
+    @@the_colors.each do |color, value|
+      self.send(:define_method, color) do
+        "\e[#{value}m" + self + "\e[0m"
+      end
+    end
+
+  end
+
+  String.create_colors
+
+  def self.colors
+    @@the_colors.keys
+  end
+
+  def self.sample_colors
+
+      @@the_colors.each do |color, value|
+        puts "\e[#{value}m" + "This is: " + "#{color.upcase}"
+      end
+
+    return nil
+  end
+
+end
+
+
+
+
+
+
+
